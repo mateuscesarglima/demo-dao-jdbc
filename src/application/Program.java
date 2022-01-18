314428ea54fd10d30267dc9b1924b2531ddc6506
Mateus Cesar <mateuscesarglima@gmail.com>
2022-01-18 08:50:04 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,19 @@
 package application;
 
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Department dp = new Department(1, "Mateus");
+		
+		Seller seller = new Seller(21,"Bob","bob@gmail.com", new Date(), 2000.00, dp);
+		
+		System.out.println(seller);
 
 	}
 
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -5,6 +5,8 @@ import java.util.Objects;
 
 public class Department implements Serializable{
 	
+	private static final long serialVersionUID = 1L;
+
 	private Integer id;
 	
 	private String name;
